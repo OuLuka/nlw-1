7ef56e7c953033d1cb7cbb5dce58dadbb93ef89c
ouluka <lucas.gouvea@upx.com>
2020-06-08 23:56:13 -0300

    Finalizando Web

--- server/src/controllers/ItemsController.ts
+++ server/src/controllers/ItemsController.ts
@@ -7,6 +7,7 @@ class ItemController {
 
     const serializedItems = items.map((item) => {
       return {
+        id: item.id,
         title: item.title,
         image_url: `http://localhost:3333/uploads/${item.image}`,
       };
